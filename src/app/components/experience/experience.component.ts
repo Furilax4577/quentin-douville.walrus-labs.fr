66fc2f9bb3f8adbd0135dd0aeed253d170de8efa
Douville Quentin <douville.quentin@gmail.com>
2025-03-14 11:14:57 +0100

    Bloc experience

--- src/app/components/experience/experience.component.ts
+++ src/app/components/experience/experience.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { EnterpriseComponent } from '../enterprise/enterprise.component';
 import { CenteringComponent } from '../centering/centering.component';
+import { TagsComponent } from '../tags/tags.component';
 
 @Component({
   selector: 'app-experience',
-  imports: [EnterpriseComponent, CenteringComponent],
+  imports: [EnterpriseComponent, CenteringComponent, TagsComponent],
   templateUrl: './experience.component.html',
   styleUrl: './experience.component.scss',
 })
